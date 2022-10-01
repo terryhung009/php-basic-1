4e1a696ecdc13941853d756ebfdf8d6092ce4731
Denny <khc741213@gmail.com>
2022-10-02 00:12:38 +0800

    Update 2.object.php

--- 2.object.php
+++ 2.object.php
@@ -1,4 +1,25 @@
 <?php
 /* 物件 */
 
-/* 物件 */
+/* 建立物件 */
+// $moana = new Person;
+
+/* 存取屬性與方法 */
+// echo "Moana is {$moana->age} years old";
+// $moana->birthday();
+// $moana->setAge(21);
+
+/* 宣告類別 */
+
+/* 宣告方法 */
+
+/* 宣告屬性 */
+
+
+
+
+
+
+
+
+
