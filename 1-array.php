9f533dbb89741822aa40f6aa1f50cb13cbce1224
Denny <khc741213@gmail.com>
2022-10-01 22:30:12 +0800

    1

--- 1-array.php
+++ 1-array.php
@@ -78,4 +78,95 @@ $value = $multi[2][0];
 // echo ("The value at row 2, column 0 is {$multi[2][0]}\n");
 
 /* 取得多個值 */
+$person = array("Fred", 35, "Betty");
+list($name, $age, $wife) = $person;
+// print_r($name.'<br>');
+// print_r($age.'<br>');
+// print_r($wife.'<br>');
+
+$person = array("Fred", 35, "Betty");
+list($name, $age) = $person;
+// print_r($name.'<br>');
+// print_r($age.'<br>');
+
+
+$values = array("hello", "world");
+// list($a, $b ,$c) = $values;
+// print_r($a.'<br>');
+// print_r($b.'<br>');
+// print_r($c.'<br>');
+
+$values = range("a", "e");
+list($m,, $n,, $o) = $values;
+// var_dump($m.'<br>', $n.'<br>',$o);
+
+
+
+
+/* 陣列切片 */
+
+$people=array("Tom", "Dick", "Harriet", "Brenda", "Jo");
+$middle=array_slice($people, 2, 2);
+// print_r($middle);
+// var_dump($middle);
+
+$people = array('name'=>'Fred', 'age'=> 35, 'wife'=> 'Betty');
+$subset = array_slice($person, 1, 2);
+// print_r($subset);
+
+$order = array("Tom", "Dick", "Harriet", "Brenda", "Jo");
+list($second, $third) = array_slice($order, 1, 2);
+// var_dump($second,$third);
+
+/* 將陣列切成小陣列 */
+// $chunks = array_chunk(array, size, [,preserve_keys]);
+
+$nums=range(1,7);
+$rows= array_chunk($nums, 3);
+// print_r($rows);
+
+/* 取得鍵和值 */
+$people = array('name'=>'Fred', 'age'=> 35, 'wife'=> 'Betty');
+$keys = array_keys($people);
+// print_r($keys);
+
+/* 檢查元素是否存在 */
+
+/* 刪除和插入元素到陣列中 */
+
+/* 轉換陣列和變數 */
+
+/* 用陣列建立變數 */
+
+/* 用變數建立陣列 */
+
+/* 迭代陣列 */
+$addresses = array("spam@cyberpromo.net","abuse@example.com");
+foreach($addresses as $value){
+  // echo "Processing {$value}\n" ;
+}
+
+
+$people = array('name'=>'Fred', 'age'=> 35, 'wife'=> 'Wilma');
+foreach($person as $key=>$value){
+  // echo "Fred's {$key} is {$value}\n";
+}
+
+/* 迭代器函式 */
+
+reset($addresses);
+while(list($key, $value) = each($addresses)){
+  // echo "{$key} is {$value}<br />\n";
+}
+
+/* 使用for 迴圈 */
+
+
+
+
+
+
+
+
+
 
